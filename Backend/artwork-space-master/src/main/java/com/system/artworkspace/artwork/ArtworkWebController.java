43ba7aa17e7a8f42cf792badc719cdd9f7508c18
Olha Horban <horbanolha64@gmail.com>
2023-11-09 14:23:59 +0200

    frontend + artwork page

--- Backend/artwork-space-master/src/main/java/com/system/artworkspace/artwork/ArtworkWebController.java
+++ Backend/artwork-space-master/src/main/java/com/system/artworkspace/artwork/ArtworkWebController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -28,5 +29,14 @@ public class ArtworkWebController {
         List <ArtworkDto> list = artworkService.getAllArtwork().stream().map(x-> ArtworkMapper.INSTANCE.artworkToArtworkDto(x)).collect(Collectors.toList());
         model.addAttribute("images", list);
         return "artwork/index";
+    }
+
+    @GetMapping("/image/{id}")
+    public String findArtworkById(@PathVariable("id") long id, Model model){
+        logger.info("Getting one artwork");
+        Artwork artwork = artworkService.findArtworkById(id);
+        ArtworkDto picture = ArtworkMapper.INSTANCE.artworkToArtworkDto(artwork);
+        model.addAttribute("picture", picture);
+        return "artwork/artwork";
     }
 }
