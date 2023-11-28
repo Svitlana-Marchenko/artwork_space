2a3f99157d5bfa032917b1b3fb3125627095fe81
Yevheniia Matsevko <mazevkozhenia@gmail.com>
2023-11-28 19:50:13 +0200

    added query to get expired exhibitions

--- Backend/artwork-space-master/src/main/java/com/system/artworkspace/exhibition/ExhibitionRepository.java
+++ Backend/artwork-space-master/src/main/java/com/system/artworkspace/exhibition/ExhibitionRepository.java
@@ -2,5 +2,9 @@ package com.system.artworkspace.exhibition;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Date;
+import java.util.List;
+
 public interface ExhibitionRepository extends JpaRepository<ExhibitionEntity, Long> {
+    List<ExhibitionEntity> findByEndDateBefore(Date date);
 }
