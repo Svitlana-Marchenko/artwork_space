bce6eb65c5c596e2e0ed23638e7be41a125430cd
Olha Horban <horbanolha64@gmail.com>
2023-12-06 00:28:51 +0200

    Exhibition creation draft

--- Frontend/artwork-space/src/mockup/mockup_exhibitions.ts
+++ Frontend/artwork-space/src/mockup/mockup_exhibitions.ts
@@ -1,13 +1,16 @@
 import {Artwork} from "./mockup_artworks";
 import {User, users} from "./mockup_users";
 
-export type Exhibition = {
-    id: number;
+export interface NewExhibition {
     curator: User;
     title: string;
     description: string;
     artworks: Artwork[];
     startDate: Date;
     endDate: Date;
+}
+
+export interface Exhibition extends NewExhibition {
+    id: number;
 }
 
