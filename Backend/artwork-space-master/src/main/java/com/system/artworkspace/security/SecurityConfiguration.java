d3b2c0e5c4c3a82fa1f92809f3d4f1f9a1dbbcec
Svitlana-Marchenko <sveta25022004@gmail.com>
2023-12-05 18:22:45 +0200

    implement deleting exhibition frontend

--- Backend/artwork-space-master/src/main/java/com/system/artworkspace/security/SecurityConfiguration.java
+++ Backend/artwork-space-master/src/main/java/com/system/artworkspace/security/SecurityConfiguration.java
@@ -1,5 +1,7 @@
 package com.system.artworkspace.security;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -12,6 +14,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 
@@ -22,6 +25,14 @@ import java.util.Collections;
 @EnableMethodSecurity
 public class SecurityConfiguration {
     //todo check security
+
+//    @Autowired
+//    private JwtAuthenticationFilter jwtAuthFilter;
+//
+//    @Autowired
+//    private AuthenticationProvider authenticationProvider;
+//
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.cors(cors -> cors
@@ -39,9 +50,9 @@ public class SecurityConfiguration {
                         //.requestMatchers("/collectioneer/auctions/**").hasAuthority("COLLECTIONEER")
                         .requestMatchers("/collections/**").hasAuthority("COLLECTIONEER")
                         .requestMatchers("/auctions/**").hasAuthority("ARTIST")
-                        .requestMatchers(HttpMethod.POST, "/exhibitions/**").hasAuthority("CURATOR")
-                        .requestMatchers(HttpMethod.PUT, "/exhibitions/**").hasAuthority("CURATOR")
-                        .requestMatchers(HttpMethod.DELETE, "/exhibitions/**").hasAuthority("CURATOR")
+//                        .requestMatchers(HttpMethod.POST, "/exhibitions/**").hasAuthority("CURATOR")
+//                        .requestMatchers(HttpMethod.PUT, "/exhibitions/**").hasAuthority("CURATOR")
+//                        .requestMatchers(HttpMethod.DELETE, "/exhibitions/**").hasAuthority("CURATOR")
                         //todo fix permitAll() for artwork
                         .requestMatchers("/artworks/**").permitAll()
                         //todo fix permitAll() for auction
--- Frontend/artwork-space/src/pages/exhibition/Exhibition.tsx
+++ Frontend/artwork-space/src/pages/exhibition/Exhibition.tsx
@@ -1,19 +1,21 @@
 import React, {useEffect, useState} from 'react';
-import {useParams} from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
 import {Exhibition as ExhibitionType} from "../../mockup/mockup_exhibitions";
 import ArtworksList from "../../components/lists/ArtworksList";
 import {Button} from "../../components/Button";
 import ExhibitionService from "../../API/ExhibitionService";
+import toast from "react-hot-toast";
 
 const Exhibition = () => {
+    const navigate = useNavigate();
     const { id } = useParams();
     //const {curator, title, description, artworks, startDate, endDate} = exhibitions[convertToInt(id)];
 
     const [exhibition, setExhibition] = useState<ExhibitionType>();
 
     const currentUser = {
-         role: "artist",
-        // role: "curator",
+        // role: "artist",
+         role: "curator",
         // role: "collectioneer",
     }
 
@@ -27,11 +29,33 @@ const Exhibition = () => {
         }
     }, [id]);
 
+    //todo change to custom window.confirm
+    //todo add link to api folder
+    function handleDelete() {
+        if (window.confirm('Are you sure you want to delete this exhibition?')) {
+            fetch(`http://localhost:8080/exhibitions/${id}`, {
+                method: 'DELETE',
+            })
+                .then((response) => {
+                    if (response.ok) {
+                        navigate(-1)
+                        toast.success('Exhibition was deleted successfully');
+                    } else {
+                        toast.error('Failed to delete exhibition.');
+                    }
+                })
+                .catch((error) => {
+                    console.error('Error while deleting exhibition:', error);
+                });
+        }
+    }
+
     //todo normal page for error
     if (!exhibition) {
         return <div>Loading...</div>;
     }
 
+    //todo відступ між кнопками Оля
 
     return (
         <div className="mx-32">
@@ -44,9 +68,15 @@ const Exhibition = () => {
                 {
                     currentUser.role === "curator"
                     ?
+                        <>
                         <div className={"w-1/4"}>
-                            <Button label={"Edit"} onClick={()=>{}}/>
+                            <>
+                                <Button label={"Edit"} onClick={()=>{}}/>
+                                <Button label={"Delete"} onClick={handleDelete} outline/>
+                            </>
                         </div>
+
+                        </>
                         :
                         null
                 }
