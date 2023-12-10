1b34beb2b85981ddeee68d33f1e5eecb50051bf5
Svitlana-Marchenko <sveta25022004@gmail.com>
2023-12-10 12:11:29 +0200

    add jwt tokens

--- Backend/artwork-space-master/src/main/java/com/system/artworkspace/security/SecurityConfiguration.java
+++ Backend/artwork-space-master/src/main/java/com/system/artworkspace/security/SecurityConfiguration.java
@@ -1,15 +1,18 @@
 package com.system.artworkspace.security;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.system.artworkspace.security.auth.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -33,41 +36,52 @@ public class SecurityConfiguration {
 //    private AuthenticationProvider authenticationProvider;
 //
 
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    public SecurityConfiguration(JwtAuthenticationFilter jwtAuthenticationFilter) {
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.cors(cors -> cors
                         .configurationSource(request -> {
                             CorsConfiguration config = new CorsConfiguration();
-                            config.setAllowedOrigins(Collections.singletonList("http://localhost:3000")); // Дозвіл на доступ з локального хоста 3000
-                            config.setAllowedMethods(Collections.singletonList("*")); // Дозволені всі HTTP-методи
-                            config.setAllowedHeaders(Collections.singletonList("*")); // Дозволені всі заголовки
+                            config.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+                            config.setAllowedMethods(Collections.singletonList("*"));
+                            config.setAllowedHeaders(Collections.singletonList("*"));
                             return config;
                         })
                 )
                 .csrf(csrf -> csrf
-                        .ignoringRequestMatchers("/users/**", "/artworks/**", "/auctions/**", "/exhibitions/**", "/collections/**", "/collectioneer/auctions/**"))
+                        .ignoringRequestMatchers("/users/**", "/artworks/**", "/auctions/**", "/exhibitions/**", "/collections/**", "/collectioneer/auctions/**", "/signin/**"))
                 .authorizeHttpRequests((authorize) -> authorize
-                        //.requestMatchers("/collectioneer/auctions/**").hasAuthority("COLLECTIONEER")
-                        .requestMatchers("/collections/**").hasAuthority("COLLECTIONEER")
-                        .requestMatchers("/auctions/**").hasAuthority("ARTIST")
+                                //.requestMatchers("/collectioneer/auctions/**").hasAuthority("COLLECTIONEER")
+                                .requestMatchers("/signin").permitAll()
+                                .requestMatchers("/collections/**").hasAuthority("COLLECTIONEER")
+                                .requestMatchers("/auctions/**").hasAuthority("ARTIST")
 //                        .requestMatchers(HttpMethod.POST, "/exhibitions/**").hasAuthority("CURATOR")
 //                        .requestMatchers(HttpMethod.PUT, "/exhibitions/**").hasAuthority("CURATOR")
 //                        .requestMatchers(HttpMethod.DELETE, "/exhibitions/**").hasAuthority("CURATOR")
-                        //todo fix permitAll() for artwork
-                        .requestMatchers("/artworks/**").permitAll()
-                        //todo fix permitAll() for auction
-                        .requestMatchers(HttpMethod.GET, "/collectioneer/auctions/**").permitAll()
-                        //todo fix permitAll() for exhibition
-                        .requestMatchers("/exhibitions/**").permitAll()
-                        //todo fix permitall for user
-                        .requestMatchers("/users/**").permitAll()
-                        .anyRequest().authenticated()
-                ).httpBasic(Customizer.withDefaults())
+                                //todo fix permitAll() for artwork
+                                .requestMatchers("/artworks/**").permitAll()
+                                //todo fix permitAll() for auction
+                                .requestMatchers(HttpMethod.GET, "/collectioneer/auctions/**").permitAll()
+                                //todo fix permitAll() for exhibition
+                                .requestMatchers("/exhibitions/**").permitAll()
+                                //todo fix permitall for user
+                                .requestMatchers("/users/**").permitAll()
+                                .anyRequest().authenticated()
+                ).sessionManagement(manager -> manager.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider()).addFilterBefore(
+                        jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class
+                )
+                .httpBasic(Customizer.withDefaults())
                 .formLogin(Customizer.withDefaults())
-        .logout(logout -> logout
-              .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-            .permitAll()
-        );
+                .logout(logout -> logout
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                        .permitAll()
+                );
         return http.build();
     }
 
@@ -90,6 +104,12 @@ public class SecurityConfiguration {
         authenticationProvider.setUserDetailsService(userDetailsService());
         authenticationProvider.setPasswordEncoder(passwordEncoder());
         return authenticationProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config)
+            throws Exception {
+        return config.getAuthenticationManager();
     }
 
 }
--- Backend/artwork-space-master/src/main/java/com/system/artworkspace/user/UserServiceImpl.java
+++ Backend/artwork-space-master/src/main/java/com/system/artworkspace/user/UserServiceImpl.java
@@ -19,6 +19,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -44,6 +45,9 @@ public class UserServiceImpl implements UserService {
     @Override
     public User createUser(User user) {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
+        if(user.getCollection() == null){
+            user.setCollection(new ArrayList<Artwork>());
+        }
         UserEntity createdUser = userRepository.save(UserMapper.INSTANCE.userToUserEntity(user));
         logger.info(CONFIDENTIAL_USER_EVENTS,"Created user with ID: {}", createdUser.getId());
         return UserMapper.INSTANCE.userEntityToUser(createdUser);
