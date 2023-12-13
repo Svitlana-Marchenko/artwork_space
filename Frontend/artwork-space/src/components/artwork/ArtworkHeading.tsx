0c00fcb37c9aac27591860b51f353725fa6c7cf8
Yevheniia Matsevko <mazevkozhenia@gmail.com>
2023-12-13 12:36:36 +0200

    fix not to show average rating if there are no ratings

--- Frontend/artwork-space/src/components/artwork/ArtworkHeading.tsx
+++ Frontend/artwork-space/src/components/artwork/ArtworkHeading.tsx
@@ -8,18 +8,21 @@ interface ArtworkHeadingProps {
     firstName: string;
     lastName: string;
     title: string;
+    showAverageRating:boolean
     averageRating: number;
 }
-const ArtworkHeading:React.FC<ArtworkHeadingProps> = ({id, username, firstName, lastName, title, averageRating}) => {
+const ArtworkHeading:React.FC<ArtworkHeadingProps> = ({id, username, firstName, lastName, title,showAverageRating, averageRating}) => {
     return (
         <div className="flex flex-col items-start">
             <UserLink id={id} username={username} />
             <p className="text-gray-400 mb-1">Painting by {firstName} {lastName}</p>
             <h2 className="text-3xl font-bold">{title.toUpperCase()}</h2>
+            {showAverageRating && (
             <div className="flex items-center gap-2">
                 <span className="text-lg font-semibold">{averageRating.toFixed(1)}</span>
                 <FontAwesomeIcon icon={solidStar} className="text-yellow-500" />
             </div>
+                )}
         </div>
     );
 };
