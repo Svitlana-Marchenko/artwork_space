b5ace22fcaad202a88b1462d0cd139f38c8e7ad6
Svitlana-Marchenko <sveta25022004@gmail.com>
2023-12-13 20:26:14 +0200

    add security test

--- Backend/artwork-space-master/src/test/java/com/system/artworkspace/SecurityTest.java
+++ Backend/artwork-space-master/src/test/java/com/system/artworkspace/SecurityTest.java
@@ -2,13 +2,11 @@ package com.system.artworkspace;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
@@ -17,7 +15,7 @@ import static org.springframework.security.test.web.servlet.setup.SecurityMockMv
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-//@ExtendWith(SpringExtension.class)
+
 @SpringBootTest
 @AutoConfigureMockMvc
 public class SecurityTest {
@@ -35,9 +33,9 @@ public class SecurityTest {
                 .build();
     }
 
-    @WithMockUser(username = "art1", password = "password", authorities = "ARTIST")
+    @WithMockUser(username = "art1", authorities = "ARTIST")
     @Test
-    public void givenPostRequestOnPrivateController_shouldReturn403() throws Exception {
+    public void testGetRequestOnPrivateControllerWithoutRightRole_shouldReturn403() throws Exception {
         for(int i=0;i<5;i++){
             mockMvc.perform(get("/exhibitions/1")
                             .contentType(MediaType.APPLICATION_JSON))
@@ -45,31 +43,79 @@ public class SecurityTest {
         }
     }
 
-    @WithMockUser(username = "art1", password = "password", authorities = "ARTIST")
     @Test
-    public void givenGetRequestOnPrivateController_shouldReturn200() throws Exception {
+    public void givenUnauthorizedUserOnGetRequestExhibition_shouldReturn200() throws Exception {
         mockMvc.perform(get("/exhibitions/1")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().is2xxSuccessful());
     }
 
     @Test
-    public void givenUnauthorizedUserOnPrivateController_expectIsUnauthorized() throws Exception {
-        mockMvc.perform(get("/artworks"))
-                .andExpect(status().isUnauthorized());
-    }
-
-    @Test
-    public void givenUnauthorizedUserOnPrivateController_expect400Error() throws Exception {
+    public void givenUnauthorizedUserOnGetRequestArtwork_shouldReturn200() throws Exception {
         mockMvc.perform(get("/artworks").contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().is4xxClientError());
+                .andExpect(status().is2xxSuccessful());
     }
 
     @WithMockUser(username = "art1", password = "password", authorities = "ARTIST")
     @Test
-    public void givenGetRequestOnPrivateControllerWithRightRole_shouldReturn200() throws Exception {
+    public void givenAuthorizedUserOnGetRequestAuction_shouldReturn200() throws Exception {
         mockMvc.perform(get("/auctions/active")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
     }
+
+    @Test
+    @WithMockUser(username = "art1", password = "password", authorities = "CURATOR")
+    public void testCuratorCanAccessExhibitions_shouldReturn200() throws Exception {
+        mockMvc.perform(get("/exhibitions/1"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser(authorities = "ARTIST")
+    public void testArtistCanAccessAuctions_shouldReturn200() throws Exception {
+        mockMvc.perform(get("/auctions/active"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser(authorities = "COLLECTIONEER")
+    public void testCollectioneerCanAccessUsers_shouldReturn200() throws Exception {
+        mockMvc.perform(get("/users/1"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testUnauthorizedPostRequestToAuction_shouldReturn500() throws Exception {
+        mockMvc.perform(post("/auctions"))
+                .andExpect(status().is4xxClientError());
+    }
+
+    @Test
+    @WithMockUser(authorities = "ARTIST")
+    public void testPostExhibitionCreationWithoutRightRole_shouldReturn400() throws Exception {
+        mockMvc.perform(post("/exhibitions"))
+                .andExpect(status().is4xxClientError());
+    }
+
+    @Test
+    @WithMockUser(authorities = "CURATOR")
+    public void testPostAuctionCreationWithoutRightRole_shouldReturn400() throws Exception {
+        mockMvc.perform(post("/auctions"))
+                .andExpect(status().is4xxClientError());
+    }
+
+    @Test
+    @WithMockUser(authorities = "COLLECTIONEER")
+    public void testPostArtworkCreationWithoutRightRole_shouldReturn400() throws Exception {
+        mockMvc.perform(post("/artworks"))
+                .andExpect(status().is4xxClientError());
+    }
+
+    @Test
+    public void testPostExhibitionCreationWithoutAuthentication_shouldReturn400() throws Exception {
+        mockMvc.perform(post("/exhibitions"))
+                .andExpect(status().is4xxClientError());
+    }
+
 }
